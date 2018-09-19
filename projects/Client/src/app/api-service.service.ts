c251490370e452e1d0fd7c5bca5fe963e614798f
Atul <atul.a@tvsnext.io>
2018-09-19 18:17:16 +0530

    JSON Changes

--- projects/Client/src/app/api-service.service.ts
+++ projects/Client/src/app/api-service.service.ts
@@ -17,7 +17,7 @@ export class ApiServiceService {
 
   getInuts(): Promise<{}> {
     return new Promise<{}>((resolve, reject) => {
-      const inputPath = this.APIEndpoint+"/5b9b566d3000002c00e7c3ad";
+      const inputPath = this.APIEndpoint+"/5ba244d32f00007e008d2ce0";
       this.http.get<{}>(inputPath).subscribe(
         data => {
           this.data = data;
