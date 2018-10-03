d015b378d22d5995164f6ed92f0d2ee45c97f99b
Veerasingam R <veerasingam.r@tvsi.local>
2018-10-03 15:05:02 +0530

    ui changes

--- projects/Login/src/app/app.module.ts
+++ projects/Login/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { NgModule } from '@angular/core';
-import { MatToolbarModule, MatNativeDateModule, MatInputModule, MatCardModule, MatFormFieldModule, MatRadioModule } from '@angular/material';
+import { MatToolbarModule, MatNativeDateModule, MatInputModule, MatCardModule, MatFormFieldModule, MatRadioModule, MatMenuModule } from '@angular/material';
 import { MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import {MatTabsModule} from '@angular/material/tabs';
 import { AppRoutingModule } from './app-routing.module';
@@ -11,13 +11,15 @@ import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { SignInComponent } from './sign-in/sign-in.component';
+import {BusniessTypeComponent} from './busniess-type/busniess-type.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HeaderComponent,
-    SignInComponent
+    SignInComponent,
+    BusniessTypeComponent
   ],
   imports: [
     BrowserAnimationsModule,
@@ -33,7 +35,8 @@ import { SignInComponent } from './sign-in/sign-in.component';
     MatInputModule,
     MatTabsModule,
     MatFormFieldModule,
-    MatRadioModule
+    MatRadioModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
