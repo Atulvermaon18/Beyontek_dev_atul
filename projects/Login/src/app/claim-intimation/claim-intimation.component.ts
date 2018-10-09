5b79125752f68100f18661b5d3a0f570fb73eb82
Veerasingam R <veerasingam.r@tvsi.local>
2018-10-09 19:07:33 +0530

    mypolicyview

--- projects/Login/src/app/claim-intimation/claim-intimation.component.ts
+++ projects/Login/src/app/claim-intimation/claim-intimation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-claim-intimation',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClaimIntimationComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
   }
-
+  newclaimFn() {
+    this.router.navigate(['/newclaim']);
+  }
 }
